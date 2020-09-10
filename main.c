bdd83934af81a3c364df274a54b6e0e804c55898
Raymond Gyabeng <raymond.gyabeng@bison.howard.edu>
2020-09-10 04:56:29 +0000

    OS:Lab 1 - Linked Lists

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "list.h"
+
+int main() {
+  printf("Tests for linked list implementation\n");
+    //list_t *l1 = list_alloc();
+  list_t * l1 = list_alloc();
+
+  return 0;
+}
