db23b0c9b5a1e1cfb6e767c10a823d243066cbba
Raymond Gyabeng <raymond.gyabeng@bison.howard.edu>
2020-09-14 01:33:02 +0000

    Implementation for linked list

--- list.c
+++ list.c
@@ -5,23 +5,196 @@
 // Raymond Gyabeng
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "list.h"
 
-list_t *list_alloc() { return NULL; }
-void list_free(list_t *l) {}
+list_t *list_alloc() { 
+  list_t * head = malloc(sizeof(list_t));
+  return head; 
+}
 
-void list_print(list_t *l) {}
-int list_length(list_t *l) { return -1; }
+void list_free(list_t *l) {
+  free(l);
+}
 
-void list_add_to_back(list_t *l, elem value) {}
-void list_add_to_front(list_t *l, elem value) {}
-void list_add_at_index(list_t *l, elem value, int index) {}
+void list_print(list_t *l) {
+    node_t * current = l->head;
+    while (current != NULL) {
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
 
-elem list_remove_from_back(list_t *l) { return -1; }
-elem list_remove_from_front(list_t *l) { return -1; }
-elem list_remove_at_index(list_t *l, int index) { return -1; }
+int list_length(list_t *l) { 
+    node_t * current = l->head;
+    int length = 0;
+    while (current != NULL) {
+      length++;
+      current = current->next;
+    }
+  return length; 
+}
 
-bool list_is_in(list_t *l, elem value) { return false; }
-elem list_get_elem_at(list_t *l, int index) { return -1; }
-int list_get_index_of(list_t *l, elem value) { return -1; }
+void list_add_to_back(list_t *l, elem value) {
+    node_t * new_node =  malloc(sizeof(node_t));
+    new_node -> value = value;
+    new_node -> next = NULL;
+    node_t* current = l->head;
+    if(current == NULL){
+      l->head = new_node;
+    }
+   else{
+    while (current->next != NULL) {
+        current = current->next;
+      }
+      current->next = new_node;
+      //new_node = current->next;
+  }
+   
+}
+
+void list_add_to_front(list_t *l, elem value) {
+    node_t * new_node =  malloc(sizeof(node_t));
+    new_node -> value = value;
+    new_node -> next = l->head;
+    l->head = new_node;
+}
+
+void list_add_at_index(list_t *l, elem value, int index) {
+  if(index == 0){
+    list_add_to_front(l, value);
+    return;
+   }
+    node_t * new_node =  malloc(sizeof(node_t));
+    new_node -> value = value;
+    new_node -> next = NULL;
+    node_t* current = l->head;
+    if(current == NULL){
+      return;
+    }
+    while (index != 0) {
+        if(index == 1){
+          node_t *tempNode = current->next;
+          current->next = new_node;
+          new_node->next = tempNode;
+          return;
+        }
+        if(current->next == NULL){
+           return;
+        }  
+        current = current->next;
+        index--;
+      }
+}
+
+elem list_remove_from_back(list_t *l) { 
+    node_t* current = l->head;
+    if(current == NULL){
+      return -1;
+    }
+    if(current->next == NULL){
+      elem retvalue = l->head->value;
+      free(l->head);
+      return retvalue;
+   }
+   else{
+    while (current->next->next != NULL) {
+        current = current->next;
+      }
+     elem retvalue = current->next->value;
+     free(current->next);
+     current->next = NULL;
+     return retvalue;
+  }
+}
+
+elem list_remove_from_front(list_t *l) { 
+  elem retvalue = -1;
+  if(l->head == NULL){
+    return retvalue;
+  }
+  
+  node_t* tempHead = l->head;
+  retvalue =  l->head->value;
+  l->head = l->head->next;
+  free(tempHead);
+  return retvalue;
+}
+
+elem list_remove_at_index(list_t *l, int index) { 
+    if(index == 0){
+    return list_remove_from_front(l);
+   }
+    node_t* current = l->head;
+    if(current == NULL){
+      return -1;
+    }
+  
+    int count = 0;
+     for (count = 0; count < index-1; count++) {
+        if(current->next == NULL){
+           return -1;
+        }  
+        current = current->next;
+      }
+  
+       node_t* temp = current->next;
+       elem retvalue =  temp->value;
+       current->next = temp->next;
+       free(temp);
+       return retvalue;
+    
+}
+
+bool list_is_in(list_t *l, elem value) { 
+  
+   node_t* current = l->head;
+   if(current == NULL){
+      return false;
+   }
+    while (current != NULL) {
+        if(current->value == value){
+          return true;
+        }
+        current = current->next;
+      }
+     return false; 
+                                       
+}
+
+elem list_get_elem_at(list_t *l, int index) { 
+  
+    node_t* current = l->head;
+    if(current == NULL){
+      return;
+    }
+  
+    int count = 0;
+     while(current != NULL) {
+        if(count == index){
+           return current->value;
+        }  
+        current = current->next;
+        count++;
+      }
+  
+       return;
+}
+
+
+int list_get_index_of(list_t *l, elem value) { 
+   node_t* current = l->head;
+   if(current == NULL){
+      return -1;
+   }
+    int index = 0;
+    while (current != NULL) {
+        if(current->value == value){
+          return index;
+        }
+        current = current->next;
+        index++;
+      }
+  
+     return -1; 
+}
--- main.c
+++ main.c
@@ -1,11 +1,86 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "list.h"
 
 int main() {
+  
   printf("Tests for linked list implementation\n");
-    //list_t *l1 = list_alloc();
-  list_t * l1 = list_alloc();
+  
+  //list should have elements 4,6,7 added and a length of 3
+  list_t * list1 = list_alloc();
+  list_add_to_back(list1, 4);
+  list_add_to_back(list1, 6);
+  list_add_to_back(list1, 7);
+	list_print(list1); 
+  printf("Length is: %d\n", list_length(list1));
+  
+  //list should have add elements 2 to front then 1 ie: 1, 2, 4, 6, 7
+  list_add_to_front(list1, 2);
+  list_add_to_front(list1, 1);
+  list_print(list1);
+  printf("Length is: %d\n", list_length(list1));
+
+  //0 should be first, 2 should be 3rd element and 25 should be last elements.
+  // ie: 0, 1, 2, 3, 4, 6, 7, 25. Length of list should be 8
+  list_add_at_index(list1, 25, 5);
+  list_add_at_index(list1, 3, 2);
+  list_add_at_index(list1, 0, 0);
+  list_print(list1);
+  printf("Length is: %d\n", list_length(list1));
+ 
+  //7 and 25 should be removed from back of list
+  // ie: 0, 1, 2, 3, 4, 6. Length of list should be 6
+  list_remove_from_back(list1);
+  list_remove_from_back(list1);
+  list_print(list1);
+  printf("Length is: %d\n", list_length(list1));
+
+  //0 and 1 should be removed from front of list
+  // ie: 2, 3, 4, 6. Length of list should be 4
+  list_remove_from_front(list1);
+  list_remove_from_front(list1);
+  list_print(list1);
+  printf("Length is: %d\n", list_length(list1));
+  
+  //Item 6 at index 3 should be removed from list
+  //No item should be removed for index 5 which is out of range
+  // ie: 2, 3, 4. Length of list should be 3
+  list_remove_at_index(list1, 3);
+  list_remove_at_index(list1, 5);
+  list_print(list1);
+  printf("Length is: %d\n", list_length(list1));
+  
+  // elem 3 and 4 should be true 
+  // elem 10 and 5 not in list should be false
+  //ie: true, false, false, true
+  bool result = list_is_in(list1, 3);
+  printf("%s", result ? "true\n" : "false\n");
+  result = list_is_in(list1, 10);
+  printf("%s", result ? "true\n" : "false\n");
+  result = list_is_in(list1, 5);
+  printf("%s", result ? "true\n" : "false\n");
+  result = list_is_in(list1, 4);
+  printf("%s", result ? "true\n" : "false\n");
+  
+  
+   //Index of 2,4,3 should be 0,2,1 respectively
+  // Elements not in list should return -1 ie. 50 and 0
+  int index = list_get_index_of(list1, 2);
+  printf("Index of 2 is: %d\n", index);
+  index = list_get_index_of(list1, 4);
+  printf("Index of 4 is: %d\n", index);
+ index = list_get_index_of(list1, 3);
+  printf("Index of 3 is: %d\n", index);
+  index = list_get_index_of(list1, 50);
+  printf("Index of 50 is: %d\n", index); 
+  index = list_get_index_of(list1, 0);
+  printf("Index of 0 is: %d\n", index);
+
+  //Element at index 0, 2 should be 2 and 4
+  int elem = list_get_elem_at(list1, 0); 
+  printf("Element at index 0 is: %d\n", elem);
+  elem =list_get_elem_at(list1, 2);
+  printf("Element at index 2 is: %d\n", elem);
 
   return 0;
 }
